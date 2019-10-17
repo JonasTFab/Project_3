c29509c3d6ae5ed77a79b9f016210359e676b90b
George Cowie <gecocowie@live.com>
2019-10-17 11:11:32 +0200

    jjj

--- Numerical_integration/plot_of_electron.py
+++ Numerical_integration/plot_of_electron.py
@@ -10,7 +10,7 @@ approx_zero = 0.01
 
 plt.plot(r,psi, label="Electron")
 #plt.plot([start,end], [approx_zero,approx_zero], label="approx zero")
-plt.plot(3,0.0025,'X', label="approx zero", color = "r")
+plt.plot(2.3,0.01,'X', label="approx zero", color = "r")
 plt.grid(); plt.legend()
 plt.xlabel("Distance r"); plt.ylabel("Psi function of electron")
 plt.show()
@@ -29,14 +29,36 @@ for i in range(size):
     N[i] = line[0]
     error[i] = line[2]
 
-plt.plot(N[1:-1],error[1:-1]); plt.grid()
-plt.title("Accuracy by number of simulations")
-plt.xlabel("N")
+
+
+
+#################################################################################
+########################Plot error as function as time###########################
+#################################################################################
+
+brute_force_time = np.array([1.9837e-05,0.00022733,0.00205109,0.0120933,0.104968,1.02758,10.2804])#time in seconds to complete the algorithm
+brute_force_error = np.array([0.173965,0.0568929,0.0860584,0.0679437,0.0849495,0.0101632,0.0042974])#error
+
+print(100000000/10000)
+improved_error = np.array([0.0256453, 0.0350268, 0.00274833, 0.000468265, 0.00141248, 0.000285713,5.96489e-05])
+improved_time = np.array([6.673e-05,0.00051678,0.00352412,0.0265326, 0.247553, 2.47919, 24.7062])
+
+print(10.2804/0.00352412)
+iterations = np.array([100,1000, 10000,100000,1000000,10000000,100000000])
+
+plt.plot(np.log10(iterations[:]),improved_error[:],"v", label = "Importance sampling",color = "r")
+plt.plot(np.log10(iterations[:]),brute_force_error[:],"o", label = "Brute Force",color = "g")
+plt.title('Error by number of iterations')
+plt.xlabel("Iterations log10(N)")
 plt.ylabel("Error")
+plt.grid()
+plt.legend()
 plt.show()
 
+plt.plot(improved_time,improved_error)
+plt.plot(brute_force_time,brute_force_error)
 
-
+#plt.show()
 
 """K = 273.15
 C_0 = 5567.11
